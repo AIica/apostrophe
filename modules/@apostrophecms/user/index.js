dcbb70d27fd1bee8af56185d9f8db45bf11109b6
AIica <alica.lancraft@gmail.com>
2023-05-04 14:43:05 +0300

    Undo user collection change

--- modules/@apostrophecms/user/index.js
+++ modules/@apostrophecms/user/index.js
@@ -246,7 +246,7 @@ module.exports = {
       // Obtain the `aposUsersSafe` MongoDB collection as `self.safe`.
 
       async ensureSafeCollection() {
-        self.safe = await self.apos.db.collection('users');
+        self.safe = await self.apos.db.collection('aposUsersSafe');
       },
 
       // Index the safe.
